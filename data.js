63cedcfa81d25d14555e0da1fa0729df948ca649
Drakening <Donatello9503@gmail.com>
2023-08-04 13:23:35 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,27 @@
+//linked to shop storing all the data in shop items. the id has to be unique. and connects to cart aswell 
+let shopItemsData =[
+    {
+    id:"number1",
+    brand:"knox hill",
+    name:"Album cover poster",
+    price:"150",
+    img:"images/f2.jpg"
+},{
+    id:"number2",
+    brand:"knox hill",
+    name:"fitted hat",
+    price:"100",
+    img:"images/f3.jpg"
+},{
+    id:"number3",
+    brand:"knox hill",
+    name:"Long sleeve ghost",
+    price:"250",
+    img:"images/f4.jpg"
+},{
+    id:"number4",
+    brand:"knox hill",
+    name:"Angel dust long sleeve",
+    price:"250",
+    img:"images/f5.jpg"
+}];
