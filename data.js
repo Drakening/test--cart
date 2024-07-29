0fc17bcc802a4682fe2f3a0202ca024d2bc9c780
Thando (Jayson) Mkhonza <95401051+Drakening@users.noreply.github.com>
2024-07-29 06:41:17 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,27 +1,32 @@
-//linked to shop storing all the data in shop items. the id has to be unique. and connects to cart aswell 
-let shopItemsData =[
+// Storing all the data in shop items
+// The id has to be unique and connects to the cart as well
+const shopItemsData = [
     {
-    id:"number1",
-    brand:"knox hill",
-    name:"Album cover poster",
-    price:"150",
-    img:"images/f2.jpg"
-},{
-    id:"number2",
-    brand:"knox hill",
-    name:"fitted hat",
-    price:"100",
-    img:"images/f3.jpg"
-},{
-    id:"number3",
-    brand:"knox hill",
-    name:"Long sleeve ghost",
-    price:"250",
-    img:"images/f4.jpg"
-},{
-    id:"number4",
-    brand:"knox hill",
-    name:"Angel dust long sleeve",
-    price:"250",
-    img:"images/f5.jpg"
-}];
+        id: "number1",
+        brand: "knox hill",
+        name: "Album cover poster",
+        price: "150",
+        img: "images/f2.jpg"
+    },
+    {
+        id: "number2",
+        brand: "knox hill",
+        name: "fitted hat",
+        price: "100",
+        img: "images/f3.jpg"
+    },
+    {
+        id: "number3",
+        brand: "knox hill",
+        name: "Long sleeve ghost",
+        price: "250",
+        img: "images/f4.jpg"
+    },
+    {
+        id: "number4",
+        brand: "knox hill",
+        name: "Angel dust long sleeve",
+        price: "250",
+        img: "images/f5.jpg"
+    }
+];
